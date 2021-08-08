a5d01e480f31c52676266cfa5651b7a230a15f37
zynastor <zynastorinc@gmail.com>
2021-08-09 03:59:43 +0900

    initialize

---
+++ src/main/kotlin/com/zynastor/homepage/controller/BoardController.kt
@@ -0,0 +1,12 @@
+package com.zynastor.homepage.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/board")
+class BoardController {
+    @GetMapping("/list")
+    fun list():String="board/list"
+}
